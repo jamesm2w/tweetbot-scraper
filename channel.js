72c012a466b6e7e953fa830d75f28303f9d3be7a
jamesm2w <11365435+jamesm2w@users.noreply.github.com>
2023-07-03 13:05:17 +0100

    Reworked scraping to use twitter embeds

--- channel.js
+++ channel.js
@@ -50,39 +50,10 @@ export class CheckUser {
             throw new Error("CheckUser instance for this channel already exists");
         }
 
-        global.checkUserInstanceList.push(this);
-
-        this.clients = this.options.webhookUrls.map(url => new WebhookClient({ url: url }));
-
-        (async () => {
-            try {
-                this.page = await this.browser.newPage();
-                await this.page.setViewport({ width: 700, height: 1024 });
-                await this.page.goto(`https://twitter.com/${this.options.channel}`, { waitUntil: "networkidle2" });
-                await this.page.waitForNetworkIdle();
-                
-                this.parseProfileInformation().then(profile => {this.profile = profile;}).catch(err => {
-                    console.warn("WARN Couldn't parse profile information for", this.options.channel, "because", err.toString());
-                    this.logClient.send(`**WARN** Couldn't parse profile information for **${this.options.channel}** because ${err}`);    
-                });
-                
-                this.lastTweet = await getLastTweet(this.options.channel, this.mongoClient);
-                this.initialised = true;
-                console.log("INFO Page", this.options.channel, "initialised successfully");
-                this.logClient.send(`**INFO** Page **${this.options.channel}** initialised successfully`);
-            } catch (err) {
-                this.initialised = false;
-                console.error("ERROR Page", this.options.channel, "not initialised because", err.toString());
-                this.logClient.send(`**ERROR** Page **${this.options.channel}** not initialised because ${err}`);
-            }
-        })();
-
         let reloadCheck = async () => {
-            // console.log("Interval Firing");
             try {
                 if (this.initialised) {
-                    // console.log("Reloading Page");
-                    await this.page.reload({ waitUntil: "networkidle2" });
+                    await this.page.reload({ waitUntil: "networkidle0" });
                     await this.check();
                 }
             } catch (err) {
@@ -90,10 +61,44 @@ export class CheckUser {
                 this.logClient.send(`**ERROR** Reload check for **${this.options.channel}** failed because ${err}`);
             }
         };
-        
-        reloadCheck().catch(console.error);
 
-        this.handle = setInterval(reloadCheck, 1000 * 60 * 2); // 2 minutes
+        (async () => {
+            try {
+                this.page = await this.browser.newPage();
+                await this.page.setViewport({ width: 700, height: 1024 });
+                
+                await this.page.goto("data:text/html;charset=utf-8," + 
+                    encodeURIComponent(`
+                        <a class="twitter-timeline" href="https://twitter.com/${this.options.channel}?ref_src=twsrc%5Etfw">
+                            Tweets by PARLYapp
+                        </a> 
+                        <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>`
+                    ), { 
+                        waitUntil: "networkidle0" 
+                    }
+                );
+                await this.page.waitForNetworkIdle();
+                this.lastTweet = await getLastTweet(this.options.channel, this.mongoClient);
+                this.initialised = true;
+                        
+                global.checkUserInstanceList.push(this);
+                this.clients = this.options.webhookUrls.map(url => new WebhookClient({ url: url }));
+        
+                console.log("INFO Page", this.options.channel, "initialised successfully");
+                this.logClient.send(`**INFO** Page **${this.options.channel}** initialised successfully`);
+
+                this.check().catch((err) => console.error("ERROR Initial check for", this.options.channel, "failed because", err.toString(), err.stack));
+
+                this.handle = setInterval(() => {
+                    reloadCheck().catch((err) => console.error("ERROR Reload check for", this.options.channel, "failed because", err.stack));
+                }, 1000 * 60 * 2); // 2 minutes
+
+            } catch (err) {
+                this.initialised = false;
+                console.error("ERROR Page", this.options.channel, "not initialised because", err.toString());
+                this.logClient.send(`**ERROR** Page **${this.options.channel}** not initialised because ${err}`);
+            }
+        })();
     }
 
     /**
@@ -118,113 +123,107 @@ export class CheckUser {
     }
 
     /**
-     * Parse profile information for stuff like display name and profile picture
-     */
-    async parseProfileInformation() {
-        await Promise.allSettled([
-            this.page.waitForSelector("div[data-testid=UserName] span"),
-            this.page.waitForSelector(`[href=\"/${this.options.channel}/photo\"] img`)
-        ]);
-
-        const name = await this.page.$eval("div[data-testid=UserName] span", el => el.innerText);
-        const profilePicture = await this.page.$eval(`[href=\"/${this.options.channel}/photo\"] img`, el => el.src);
-        return { name, profilePicture };
-    }
-
-    /**
      * Scans page for tweet elements and posts new ones to Discord
      */
     async check() {
-        // console.log("Performing Check for New Tweets");
-
-        await this.page.waitForSelector("[data-testid=tweet]");
-
-        if (this.profile === undefined) {
-            this.parseProfileInformation().then(profile => {this.profile = profile;}).catch(err => {
-                // console.warn("WARN Couldn't parse profile information for", this.options.channel, "because", err.toString());
-                // this.logClient.send(`**WARN** Couldn't parse profile information for **${this.options.channel}** because ${err}`);    
-            });
-        }
-
         if (this.options.screenshot) {
             this.page.screenshot({ path: `./output/screenshot-${this.options.channel}-${(new Date()).getTime()}.png` })
                 .catch(console.error);
         }
 
-        const tweets = await this.page.$$("[data-testid=tweet]");
-        // console.log("Found", tweets.length, "tweets");
+        // console.log(`${this.options.channel} Starting Check`);
+
+        const twitterFrameHandle = await this.page.waitForSelector("iframe");
+        const frame = await twitterFrameHandle.contentFrame();
+        // console.log(`${this.options.channel} Found Frame`);
+
+        await frame.waitForSelector("article");
+        let tweets = await frame.$$("article");
+
+        // console.log(`${this.options.channel} Found `, tweets.length, "tweets");
         tweets.reverse();
 
         for (let tweet of tweets) {
-            // await Promise.allSettled([
-            //     tweet.waitForSelector("[data-testid=socialContext]"),
-            //     tweet.waitForSelector("div[data-testid=tweetText] span"),
-            //     tweet.waitForSelector("div[data-testid=User-Name] a")
-            // ]);
-            // Need to determine (a) context of tweet (b) if the tweet is after the last seen tweet 
-            const context = await tweet.$$eval("[data-testid=socialContext]", els => els.map(el => el.textContent)); // pinned, promoted, reply, retweet, tweet?
-            const content = await tweet.$eval("div[data-testid=tweetText] span", el => el.textContent);
+            let profilePicture = await tweet.$("[data-testid=Tweet-User-Avatar] img");
+            let userNameLinks = await tweet.$$("[data-testid=User-Name] a");
 
-            const isRetweet = context.some(c => c.includes("Retweeted"));
-            const isPinned = context.some(c => c.includes("Pinned Tweet"));
+            let profileSrc = profilePicture ? await profilePicture.getProperty("src") : null;
+            let displayName = userNameLinks ? await userNameLinks[0].getProperty("innerText") : null;
+            let userName = userNameLinks ? await userNameLinks[1].getProperty("innerText") : null;
+            let tweetLink = userNameLinks ? await userNameLinks[2].getProperty("href") : null;
 
-            const urls = await tweet.$$eval("div[data-testid=User-Name] a", els => els.map(el => el.href) );
-            for (let url of urls) {        
-                const id = url.match(/https:\/\/.*?\/status\/(\d+)/);
-                if (id) {
-                    const postURL = id[1];
-                    const postTime = snowflakeToTimestamp(postURL);
-                    const shouldntPost = (this.lastTweet && Math.floor(postTime) <= Math.ceil(snowflakeToTimestamp(this.lastTweet)) + 1) 
-                        || isPinned
-                        || this.seenTweets.includes(postURL);
+            if (tweetLink == null) {
+                console.log("WARN Couldn't find tweet link for", this.options.channel);
+                this.logClient.send(`**WARN** Couldn't find tweet link for **${this.options.channel}**`);
+                continue;
+            }
 
-                    if (process.env.NODE_ENV != "production") {
-                        console.log("Found Tweet", id[1], "Should Post", !shouldntPost);
-                        console.log("\tTimestamp", postTime, new Date(postTime).toLocaleTimeString(), "Last Tweet", snowflakeToTimestamp(this.lastTweet), new Date(snowflakeToTimestamp(this.lastTweet)).toLocaleTimeString());
-                        console.log("\tContext", context);
-                        console.log("\tContent", content);    
-                    }
+            profileSrc = profileSrc ? await profileSrc.jsonValue() : "https://discord.com/assets/1f0bfc0865d324c2587920a7d80c609b.png";
+            displayName = displayName ? await displayName.jsonValue() : "Twitter";
+            userName = displayName ? await userName.jsonValue() : "twitter";
 
-                    // if posted before last tweet we saw, skip. if pinned, skip. if already seen, skip.
-                    if (shouldntPost) {
-                        continue; // skip this tweet
-                    }
+            // Strip query string of tweet link
+            tweetLink = (await tweetLink.jsonValue()).toString().split("?")[0];
+            // Replace twitter.com with vxtwitter.com
+            tweetLink = tweetLink.replace("twitter.com", "vxtwitter.com");
 
-                    this.seenTweets.push(postURL);
-                    console.log("New Tweet", this.options.channel, postURL);
-                    // send POST request to Discord webhook url
-                    // for (let webhookUrl of this.options.webhookUrls) {
-                    //     fetch(webhookUrl, { 
-                    //         method: "POST", 
-                    //         headers: { "Content-Type": "application/json" },
-                    //         body: JSON.stringify({ content: `${isRetweet ? "RT" : ""} https://twitter.com/${this.options.channel}/status/${postURL}` })
-                    //     }).catch(console.error);    
-                    // }
+            // isRetweet true if a retweet. Retweets have different handle than channel name, if an error means it's just twitter, don't assume retweet.
+            let isRetweet = userName != "twitter" && userName != `@${this.options.channel}`;
 
-                    this.clients.forEach(client => {
-                        client.send({
-                            username: this.profile ? this.profile.name : "Twitter",
-                            avatarURL: this.profile ? this.profile.profilePicture : "https://discord.com/assets/1f0bfc0865d324c2587920a7d80c609b.png",
-                            content: `${isRetweet ? "RT" : ""} https://twitter.com/${this.options.channel}/status/${postURL}`
-                        }).catch(err => {
-                            console.warn("WARN Couldn't send message to webhook", err);
-                            this.logClient.send(`**WARN** Couldn't send message to webhook \`${client.url}\` for **${this.options.channel}** because ${err}`);
-                        });
-                    });
-                    
-                    this.lastTweet = postURL;
+            const id = tweetLink.match(/https:\/\/.*?\/status\/(\d+)/);
+            const postURL = id[1];
+            const postTime = snowflakeToTimestamp(postURL);
+            const shouldntPost = (this.lastTweet && Math.floor(postTime) <= Math.ceil(snowflakeToTimestamp(this.lastTweet)) + 1) 
+                || this.seenTweets.includes(postURL);
 
-                    const db = this.mongoClient.db("tweetbotv2");
-                    const channelData = db.collection("channelData");
+            if (process.env.NODE_ENV != "production") {
+                console.log("-- Found Tweet", id[1], "Should Post", !shouldntPost);
+                console.log("Timestamp", postTime, new Date(postTime).toLocaleTimeString(), "Last Tweet", snowflakeToTimestamp(this.lastTweet), new Date(snowflakeToTimestamp(this.lastTweet)).toLocaleTimeString());
+                console.log({
+                    isRetweet,
+                    profileSrc,
+                    displayName,
+                    userName,
+                    tweetLink
+                });
+            }
 
-                    await channelData.updateOne({ channel: this.options.channel }, { $set: { lastTweet: postURL } }, { upsert: true });
+            // if posted before last tweet we saw, skip. if pinned, skip. if already seen, skip.
+            if (shouldntPost) {
+                continue; // skip this tweet
+            }
 
-                    if (this.seenTweets.length > 10) {
-                        this.seenTweets.shift();
-                    }
+            this.seenTweets.push(postURL);
+            console.log("New Tweet", this.options.channel, tweetLink);
+            // send POST request to Discord webhook url
+            
+            let usernameString;
+            if (isRetweet) {
+                usernameString = `${displayName} ${userName} - @${this.options.channel}`;
+            } else {
+                usernameString = `${displayName} ${userName}`;
+            }
 
-                    break; // break out of URL loop
-                }
+            this.clients.forEach(client => {
+                client.send({
+                    username: this.options.channel ? usernameString : "Twitter",
+                    avatarURL: profileSrc ? profileSrc : "https://discord.com/assets/1f0bfc0865d324c2587920a7d80c609b.png",
+                    content: `${isRetweet ? "RT" : ""} ${tweetLink}`
+                }).catch(err => {
+                    console.warn("WARN Couldn't send message to webhook", err);
+                    this.logClient.send(`**WARN** Couldn't send message to webhook \`${client.url}\` for **${this.options.channel}** because ${err}`);
+                });
+            });
+            
+            this.lastTweet = postURL;
+
+            const db = this.mongoClient.db("tweetbotv2");
+            const channelData = db.collection("channelData");
+
+            await channelData.updateOne({ channel: this.options.channel }, { $set: { lastTweet: postURL } }, { upsert: true });
+
+            if (this.seenTweets.length > 10) {
+                this.seenTweets.shift();
             }
         } 
     }
